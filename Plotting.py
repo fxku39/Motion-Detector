64d5345b438fe4a9e22c845dcfc73c2c33b4d0b9
fxku39 <fxkundo9932@gmail.com>
2024-11-09 16:18:59 -0300

    Update Plotting.py
    We adjusted the code so now it can show correctly the datetime in the labels when we hover the mouse accross the ploy

--- Plotting.py
+++ Plotting.py
@@ -2,7 +2,11 @@ from Motion_detector import df
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import HoverTool, ColumnDataSource
 
+#We create new columns in the df utilizing the already existing columns 'start' and 'end'. The new columns then have the datetime formated and passed to the hover tool
+df['Start_string']=df['start'].dt.strftime('%Y-%m-%d %H:%M:%S')
+df['End_string']=df['end'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
+cds=ColumnDataSource(df)
 
 #In this block of code we create a figure object with the x axis defined as datetime type, then we adjust the size of our plot box and add a title. At last we eliminate the ticks in y-axis
 # and change the value of y axis to 0 and 1
@@ -11,12 +15,12 @@ p.yaxis.minor_tick_line_color=None
 p.yaxis.ticker.desired_num_ticks = 1
 
 # We create a hovertool object and add the tooltips so we can add a label when we hover the mouse over the graphic
-hover=HoverTool(tooltips=[('Start', '@start'),('End','@end')])
+hover=HoverTool(tooltips=[('Start', '@Start_string'),('End','@End_string')])
 p.add_tools(hover)
 
 
 #Creation of the quad plot
-q=p.quad(left=df['start'], right=df['end'], bottom=0, top=1, color='green')
+q=p.quad(left='start', right='end', bottom=0, top=1, color='green', source=cds)
 
 output_file('Motion_Graph.html')
 
